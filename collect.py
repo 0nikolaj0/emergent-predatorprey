70d9bdb8058a2babc66c7ff6d3d1677b890c636d
Nikolaj Wilms <nikolaj.wilms@gmail.com>
2023-04-03 14:43:09 +0200

    metric for games, cluster (visualization), multiple plots

--- collect.py
+++ collect.py
@@ -23,15 +23,24 @@ def recordGameData(num_agent, num_prey):
     data = torch.Tensor(configs.DEFAULT_TIME_HORIZON, con.batch_size, num_agent+num_prey, 2)
     for val in range(configs.DEFAULT_TIME_HORIZON):
         data[val] = timesteps[val]['locations']
-    torch.save(data, f"data\{num_agent}{num_prey}.pt")
+    torch.save(data, f"data/{num_agent}{num_prey}.pt")
 
-def kclusterGame(path, num):
+def getGameMetrics(path):
+    num_agent = int(path[0])
+    num_prey  = int(path[1])
+    gamedata = torch.flatten(torch.load(f'data/{path}'),end_dim=1)
+    metric = torch.Tensor(gamedata.size()[0],2)
+    for i in range(gamedata.size()[0]):
+        agents = gamedata[i,:num_agent,:]
+        metric[i,0] = torch.mean(torch.cdist(agents,gamedata[i,num_prey+num_agent-1:,:],1))
+        metric[i,1] = torch.mean(torch.cdist(agents,agents,1))
+    torch.save(metric, f"data/metric{path}")
+
+def kcluster(path, num_clusters):
     file = torch.load(f'data/{path}')
-    gamedata = torch.flatten(file,end_dim=2)
-    num_clusters = num
     device = torch.device('cpu')
     result = kmeans(
-    X=gamedata, num_clusters=num_clusters, distance='euclidean', device=device
+    X=file, num_clusters=num_clusters, distance='euclidean', device=device
     )
     torch.save(result, f'data/cluster{path}')
 
@@ -41,10 +50,10 @@ def visualizeClusters(path):
     cluster_ids_x_und, cluster_centers_und = f2
     cluster_ids_x = cluster_ids_x_und.detach()
     cluster_centers = cluster_centers_und.detach()
-    x = torch.flatten(f1.detach(),end_dim=2)
+    x = f1.detach()
 
     plt.figure(figsize=(4, 3), dpi=160)
-    plt.scatter(x[:, 0], x[:, 1], c=cluster_ids_x, cmap='cool')
+    plt.scatter(x[:, 0], x[:, 1], c=cluster_ids_x, cmap='plasma')
     plt.scatter(
         cluster_centers[:, 0], cluster_centers[:, 1],
         c='white',
@@ -52,9 +61,44 @@ def visualizeClusters(path):
         edgecolors='black',
         linewidths=2
     )
-    plt.axis([-4, 20, -4, 20])
+    plt.axis([torch.min(x[:,0])-1, torch.max(x[:,0])+1, torch.min(x[:,1])-1, torch.max(x[:,1])+1])
+    plt.xlabel("mean distance agent to prey")
+    plt.ylabel("mean distance between agents")
     plt.tight_layout()
     plt.show()
 
-kclusterGame('21.pt', 20)
-visualizeClusters('21.pt')
+# recordGameData(2,2)
+# getGameMetrics('22.pt')
+# kcluster('metric31.pt', 6)
+# visualizeClusters('metric31.pt')
+
+def pipeline(num_agent, num_prey, num_cluster):
+    kcluster(f'metric{num_agent}{num_prey}.pt',num_cluster)
+    visualizeClusters(f'metric{num_agent}{num_prey}.pt')
+
+def visualizeMultiple(paths):
+    fig, axs = plt.subplots(2,2)
+    for i, path in enumerate(paths):
+        ax = axs.flat[i]
+        f1 = torch.load(f'data/{path}')
+        f2 = torch.load(f'data/cluster{path}')
+        cluster_ids_x_und, cluster_centers_und = f2
+        cluster_ids_x = cluster_ids_x_und.detach()
+        cluster_centers = cluster_centers_und.detach()
+        x = f1.detach()
+
+        ax.scatter(x[:, 0], x[:, 1], c=cluster_ids_x, cmap='plasma')
+        ax.scatter(
+            cluster_centers[:, 0], cluster_centers[:, 1],
+            c='white',
+            alpha=0.6,
+            edgecolors='black',
+            linewidths=2
+        )
+        ax.axis([torch.min(x[:,0])-1, torch.max(x[:,0])+1, torch.min(x[:,1])-1, torch.max(x[:,1])+1])
+        ax.set_title("mean distance agent to prey")
+        ax.set_ylabel("mean distance between agents")
+    plt.show()
+
+visualizeMultiple(['metric21.pt','metric22.pt', 'metric31.pt', 'metric32.pt'])
+#pipeline(2,2,6)
