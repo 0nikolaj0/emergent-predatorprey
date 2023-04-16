7bb2812d6a0552f842cf4a82614a558683811766
Nikolaj Wilms <nikolaj.wilms@gmail.com>
2023-04-16 16:52:40 +0200

    data collection changes

--- collect.py
+++ collect.py
@@ -45,12 +45,14 @@ def record_game_utter(num_agent, num_prey, save=False): #records game locations 
     return locationdata, utterdata
 
 def get_game_metrics(paths, save=False): #for each step in a game in a batch, computes the metrics
-    g = torch.flatten(torch.load(f'data/{paths[0]}'),end_dim=1) #we define 2 metrics for a game:
+    locd, utterdata = torch.load(f'data/utter/{paths[0]}')
+    g = torch.flatten(locd,end_dim=1) #we define 2 metrics for a game:
     full = torch.Tensor(len(paths),g.size()[0],2)               #-mean of distance between agents
     for ind, path in enumerate(paths):                          #-mean of distance to the closest prey for each agent
         num_agent = int(path[0])
         num_prey  = int(path[1])
-        gamedata = torch.flatten(torch.load(f'data/{path}'),end_dim=1)
+        gamed, utterd = torch.load(f'data/utter/{path}')
+        gamedata = torch.flatten(gamed,end_dim=1)
         metric = torch.Tensor(gamedata.size()[0],2)
         for i in range(gamedata.size()[0]):
             agents = gamedata[i,:num_agent,:]
@@ -59,22 +61,22 @@ def get_game_metrics(paths, save=False): #for each step in a game in a batch, co
         full[ind] = metric
     result = full.flatten(end_dim=1)
     if save:
-        torch.save(result, f"data/metric{paths}.pt")
+        torch.save(result, f"data/utter/metricm.pt")
     return result
 
 def kcluster(path, num_clusters, save=False): #clusters game metrics
-    file = torch.load(f'data/{path}')
+    file = torch.load(f'data/utter/{path}')
     device = torch.device('cpu')
     result = kmeans(
         X=file, num_clusters=num_clusters, distance='euclidean', device=device
     )
     if save:
-        torch.save(result, f'data/cluster{path}')
+        torch.save(result, f'data/utter/clustermetricm.pt')
     return result
 
 def visualize_clusters(path): #visualizes clustered game metrics
-    f1 = torch.load(f'data/{path}')
-    f2 = torch.load(f'data/cluster{path}')
+    f1 = torch.load(f'data/utter/{path}')
+    f2 = torch.load(f'data/utter/cluster{path}')
     cluster_ids_x_und, cluster_centers_und = f2
     cluster_ids_x = cluster_ids_x_und.detach()
     cluster_centers = cluster_centers_und.detach()
@@ -126,22 +128,24 @@ def visualize_multiple(paths): #visualizes multiple clustered game metrics
     fig.tight_layout()
     plt.show()
 
-def plot_losses(path): #plots losses for each epoch from a file
+def plot_losses(paths): #plots losses for each epoch from a file
     fig, ax = plt.subplots()
-    data = np.load(f'lossdata/{path}.npy')
-    min_agents = int(path[0])
-    max_agents = int(path[1])
-    min_preys = int(path[2])
-    max_preys = int(path[3])
-    num_epochs = int(path[4:])
+    for path in paths:
+        data = np.load(f'lossdata/{path}.npy')
+        min_agents = int(path[0])
+        max_agents = int(path[1])
+        min_preys = int(path[2])
+        max_preys = int(path[3])
+        num_epochs = int(path[4:7])
 
-    for a in range(min_agents, max_agents + 1):
-        for l in range(min_preys, max_preys + 1):
-            plt.plot(np.arange(num_epochs), data[a-1][l-1], label=f"num_agents: {a} num_prey: {l}")
+        for a in range(min_agents, max_agents + 1):
+            for l in range(min_preys, max_preys + 1):
+                plt.plot(np.arange(num_epochs), data[a-1][l-1], label=f"{path}: num_agents: {a} num_prey: {l}")
 
     ticks = np.arange(0,num_epochs+1,num_epochs/10)
     plt.legend(loc="upper left")
     #ax.set_yscale('log')
+    plt.grid()
     ax.set_xticks(ticks)
     plt.show()
 
@@ -151,10 +155,20 @@ def predict_cluster(pathc, pathg): #for a game location data file, computes the 
     prediction = kmeans_predict(metrics, cluster_centers)
     return pathg, prediction
 
-def kl_diverg():
-    return
+def complete_data_set():
+    locd, utterd = torch.load(f'data/utter/21utter.pt')
+    metric = torch.load(f'data/utter/metricm.pt')[:locd.size()[0]*locd.size()[1],:]
+    return torch.flatten(locd, end_dim=1), torch.flatten(utterd, end_dim=1), metric
     
-#visualizeClusters('metricmultiple.pt')
-#plotLosses('3412100')
+#visualize_clusters('metricm.pt')
+plot_losses(['2312100new'])
 #record_game_utter(2,1,save=True)
-record_game_utter(3,2,save=True)
+#record_game_utter(3,2,save=True)
+#get_game_metrics(['21utter.pt', '22utter.pt', '31utter.pt', '32utter.pt'],save=True)
+#x,y,z = complete_data_set()
+#print(x.size(),y.size(),z.size())
+#visualize_multiple(['metric21.pt', 'metric22.pt', 'metric31.pt', 'metric32.pt'])
+#kcluster('metricm.pt',7,save=True)
+#visualize_clusters('metricm.pt')
+#x = torch.load(f'data/utter/21utter.pt')
+#print(x.size())
